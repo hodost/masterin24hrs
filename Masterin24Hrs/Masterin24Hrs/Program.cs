ccdcf2e068ca02e9dd1cbe97253bad2e9e61b4ca
hodost <hodost@outlook.hu>
2019-03-16 12:06:11 +0100

    Github gyak és projekt létrehozás

---
+++ Masterin24Hrs/Masterin24Hrs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masterin24Hrs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
