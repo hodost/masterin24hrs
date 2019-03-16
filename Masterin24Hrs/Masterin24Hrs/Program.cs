155f7b0fca38d3ebce8c9d041e4fbff9ffe26b01
hodost <hodost@outlook.hu>
2019-03-16 12:22:42 +0100

    #2: Kör területe

--- Masterin24Hrs/Masterin24Hrs/Program.cs
+++ Masterin24Hrs/Masterin24Hrs/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Masterin24Hrs
 {
-    class Program
+    class Area  // Kör területének számítása
     {
-        static void Main(string[] args) // Megjegyzés
+        static void Main()
         {
+            // Változók bevezetése
+            int radius = 4;     // sugár, r
+            const double pi = 3.14159;      // pí
+            double area;    // terület
+
+            // Számítás (kör területe = r négyzet * pí)
+            area = (radius * radius) * pi;
+
+            // Eredmény kiírása
+            Console.WriteLine("Sugár: {0}, Pí: {1}", radius, pi);
+            Console.WriteLine("A köre területe: {0}", area);
+            Console.ReadKey();
         }
     }
 }
