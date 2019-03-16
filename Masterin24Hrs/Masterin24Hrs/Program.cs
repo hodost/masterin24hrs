1c5c7695d754b1efd17a106c99e5f7b8cca05081
hodost <hodost@outlook.hu>
2019-03-16 16:30:25 +0100

    #2 Példaprogramok

--- Masterin24Hrs/Masterin24Hrs/Program.cs
+++ Masterin24Hrs/Masterin24Hrs/Program.cs
@@ -1,23 +1,94 @@
-﻿using System;
+﻿using System;   // A System névtér használata
 
 namespace Masterin24Hrs
 {
-    class Area  // Kör területének számítása
+    /* Area
+    class Area  // Osztálydeklaráció. Bevezetjük az Area osztályt
     {
-        static void Main()
+        static void Main()  // Ez a program kiindulópontja
         {
-            // Változók bevezetése
+            // Deklarációk | változók bevezetése
             int radius = 4;     // sugár, r
             const double pi = 3.14159;      // pí
             double area;    // terület
 
-            // Számítás (kör területe = r négyzet * pí)
+            // Értékadó utasítás | számítás (kör területe = r négyzet * pí)
             area = (radius * radius) * pi;
 
-            // Eredmény kiírása
+            // Függvényhívás | eredmény kiírása
             Console.WriteLine("Sugár: {0}, Pí: {1}", radius, pi);
             Console.WriteLine("A köre területe: {0}", area);
             Console.ReadKey();
         }
     }
+    */
+
+    /* varValues
+    class varValues  // Osztálydeklaráció. Bevezetjük az Area osztályt
+    {
+        static void Main()  // Ez a program kiindulópontja
+        {
+            // firstVar bevezetése
+            int firstVar;
+
+            // secondVar bevezetése
+            int secondVar = 200;
+
+            // Értékadás
+            firstVar = 5;
+
+            // Érték kiírása
+            Console.WriteLine("\nfirstVar értéke: {0}", firstVar);
+            Console.WriteLine("secondVar értéke: {0}", secondVar);
+
+            // Új érték hozzárendelése:
+            firstVar = 1010;
+            secondVar = 2020;
+
+            // Új érték kiírása
+            Console.WriteLine("\nfirstVar új értéke: {0}", firstVar);
+            Console.WriteLine("secondVar új értéke: {0}", secondVar);
+
+            Console.ReadKey();
+        }
+    }
+    */
+
+    /* Chars
+    class Chars
+    {
+        public static void Main()
+        {
+            int ctr;
+            char ch;
+
+            Console.WriteLine("\nNumber \t Value\n");
+
+            for(ctr = 63; ctr <= 94; ctr = ctr + 1)
+            {
+                ch = (char) ctr;
+                Console.WriteLine("{0}    is    {1}", ctr, ch);
+            }
+            Console.ReadKey();
+        }
+    }
+    */
+
+    /* Gyak1
+    class Gyak1
+    {
+        static void Main()
+        {
+            int ctr;
+
+            for (ctr = 1; ctr <= 10; ctr++)
+            {
+                Console.Write("{0:D3} ", ctr);
+            }
+            Console.ReadKey();
+        }
+    }
+    */
+
+    
 }
