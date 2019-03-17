4f7c67781a71b1d66a05dd52e4226adcf09fef0d
hodost <hodost@outlook.hu>
2019-03-17 21:31:48 +0100

    #3 Művelete C#-ban

--- Masterin24Hrs/Masterin24Hrs/Program.cs
+++ Masterin24Hrs/Masterin24Hrs/Program.cs
@@ -90,5 +90,111 @@ namespace Masterin24Hrs
     }
     */
 
+    // Day 3 ***
+
+    /* Pie
+    class Pie
+    {
+        static void Main()
+        {
+            int forMe = 0;
+            int piecesOfPie = 0;
+
+            piecesOfPie = 3 * 6;
+            forMe = piecesOfPie % 13;
+
+            Console.WriteLine("Szeletek száma: {0} db", piecesOfPie);
+            Console.WriteLine("Nekem jut: {0} db", forMe);
+            Console.ReadKey();
+        }
+    }
+    */
+
+    /* prePost
+    class prePost
+    {
+        static void Main()
+        {
+            int a = 1;
+            int b = 1;
+
+            Console.WriteLine("Kiinduló értékek: a = {0}, b = {1}", a, b);
+            Console.WriteLine("Az a-t kiírjuk, majd növeljük eggyel, a b-t növeljük eggyel, majd kiírjuk.");
+            Console.WriteLine();
+            Console.WriteLine("a++: {0}, ++b: {1}", a++, ++b);  // Az a-t kiírjuk, majd növeljük eggyel, a b-t növeljük eggyel, majd kiírjuk
+            Console.WriteLine("a++: {0}, ++b: {1}", a++, ++b);
+            Console.WriteLine("a++: {0}, ++b: {1}", a++, ++b);
+            Console.WriteLine();
+            Console.WriteLine("a--: {0}, --b: {1}", a--, --b);
+            Console.WriteLine("a--: {0}, --b: {1}", a--, --b);
+            Console.WriteLine("a--: {0}, --b: {1}", a--, --b);
+            Console.WriteLine();
+            Console.WriteLine("Végső értékek: a: {0}, b: {1}", a, b);
+
+            Console.ReadLine();
+        }
+    }
+    */
+
+    /* ifTest
+class ifTest
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 0;
+
+        Console.WriteLine("IF parancs:");
+
+        if (a == b) // Ha itt kiteszem a pontosvesszőt (hibásan), akkor lefut az IF parancs akkor is, ha a feltétetl nem teljesül
+        {
+            Console.WriteLine("Az a egyenlő b, tehát az IF feltétel teljesült, azaz IGAZ volt, így lefutott az IF parancs.");
+        }
+        Console.WriteLine("IF utáni rész.");
+        Console.ReadKey();
+    }
+}
+      */
     
+    /* ifTest2
+    class ifTest2
+    {
+        static void Main()
+        {
+            int day = 4;
+            char sex = 'f';
+
+            Console.WriteLine("Teszt indítása.. (day: {0}, sex: {1})", day, sex);
+            Console.WriteLine();
+
+            if (day >= 1 && day <= 7)
+            {
+                Console.WriteLine("Day 1-7 -ig");
+            }
+
+            if (sex == 'm' || sex == 'f')
+            {
+                Console.WriteLine("Sex \"m\" vagy \"f\"");
+            }
+            Console.WriteLine();
+            Console.WriteLine("IF utáni rész.");
+            Console.ReadKey();
+        }
+    }
+    */
+
+    class ifShort
+    {
+        static void Main()
+        {
+            int a = 1;
+            int b = 0;
+            int allitas;
+
+            allitas = (a == b) ? 1 : 0;
+            Console.WriteLine("Az állítás: {0}", allitas);
+            Console.WriteLine("mert a = {0}, és b = {1}.",a, b);
+            Console.ReadKey();
+        }
+    }
 }
